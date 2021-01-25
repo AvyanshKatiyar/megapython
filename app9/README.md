1c084dfb7920a4f9a5b3fa8cebdeeaac3b3e94d4
Avyansh Katiyar <avyanshkatiyar@gmail.com>
2021-01-26 01:47:00 +0530

    first commit

---
+++ app9/README.md
@@ -0,0 +1,32 @@
+#Setting up heroku
+
+you need to set up a git repository. 3 files requirements.txt, proc, runtime.txt. 
+
+Also a small change in app.py file. Change local database postgres to Heroku Database. 
+
+So need to create a heroku.
+
+heroku login.
+
+Database is an add on.
+
+USE CLI to make database.
+ 
+ >heroku create avidbase
+
+ >  heroku-postgresql:hobby-dev --app avidbase--app avidbase
+ 10,000 records cap
+
+ Method to get the URI 
+
+ > heroku config --app avidbase
+
+
+postgres://xvelxqcbmxgxjd:9dd39d36b58835f3939857f58f40765d3a39c747889d7598cf2fe7f207418331@ec2-3-214-4-151.compute-1.amazonaws.com:5432/d2kfnk64rtkhr8
+
+## Making the req files
+
+virtual environment first
+
+install gunicornss
+pip freeze > requirements.txt
--- app9/app.py
+++ app9/app.py
@@ -5,13 +5,22 @@ from flask import Flask, render_template, request
 
 #note ext has been deprectiated
 from flask_sqlalchemy import SQLAlchemy
-
 #__name__ is the name of the current Python module
+from send_email import send_email
+
+#average 
+from sqlalchemy.sql import func
+
+
 app=Flask(__name__)
 
 #making connection to the database
-app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:postgres1234@localhost:5432/height_collector"
+#app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:postgres1234@localhost:5433/height_collector"
+#local host was missing
 
+#heroku dbase
+#?sslmode=require
+app.config["SQLALCHEMY_DATABASE_URI"]="postgres://xvelxqcbmxgxjd:9dd39d36b58835f3939857f58f40765d3a39c747889d7598cf2fe7f207418331@ec2-3-214-4-151.compute-1.amazonaws.com:5432/d2kfnk64rtkhr8?sslmode=require"
 db=SQLAlchemy(app)
 
 class Data(db.Model):
@@ -34,7 +43,7 @@ class Data(db.Model):
 #command line
 # . virtual/bin/activate
 # from app import db
-# 
+#db.create_all()
 
 
 
@@ -46,11 +55,35 @@ def index():
 @app.route("/success", methods=['POST'])
 def success():
     if request.method=="POST":
+        
         email=request.form["email_name"]
         height=request.form["height_name"]
-        print(email)
-    return render_template("success.html")
+        #print(email, height)
+        
 
+        # delete from data where email_='email' NOTE that the single quotes are very important
+        # select * from data to get query as object is data
+        #adding to the database
+            
+            # gives number of email already in email 
+        if db.session.query(Data).filter(Data.email_==email).count() ==0:   
+            data=Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            
+
+
+            #finding the average value
+            average_height=db.session.query(func.avg(Data.height_)).scalar()
+            average_height=round(average_height, 1)
+            print(average_height)
+            count=db.session.query(Data.height_).count()
+            send_email(email, height,average_height, count )
+
+            return render_template("success.html")
+        else:
+            return render_template("index.html", text= "Moshi moshi, already have your height")
+            #NOTE if change does not show up then clear browser cache
 #if script is being executed and not imported  i.e is 
 # the main script
 if __name__== "__main__":
